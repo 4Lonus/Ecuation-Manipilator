73178e0b4b946a9356a2dbb8cb499e25ddb95ed0
4Lonus <lopezjpll06@gmail.com>
2026-01-13 14:34:43 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,11 +3,12 @@
 #include <memory>
 
 int main() {
-    auto x = Rational::create(35, 30);
+    auto x = Rational::create((7*5)*2, (5*3)*2);
 
     auto simplifiedX = x->simplify();
 
+    COut::println(x->toString());
     COut::println(simplifiedX->toString());
 
-    return 1;
+    return 0;
 }
